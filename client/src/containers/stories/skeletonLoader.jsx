fde22be4a8f24bbda2e474f63ee6a08c59731ae0
Yash Kamnani <yash@simplogics.com>
2024-12-08 21:47:46 +0530

    bug updates

--- client/src/containers/stories/skeletonLoader.jsx
+++ client/src/containers/stories/skeletonLoader.jsx
@@ -9,7 +9,7 @@ export const SkeletonLoader = () => (
           <div className="story_card_title skeleton_title"></div>
           <div className="story_card_description skeleton_description"></div>
         </div>
-        <div className="flex px-4">
+        <div className="skeleton_btn_container">
           <div className="primary_btn skeleton_btn mr-3"></div>
           <div className="secondary_btn skeleton_btn"></div>
         </div>
