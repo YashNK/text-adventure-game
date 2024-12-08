35a8fa14adb083ff062b1a2714d690daf4297562
Yash Kamnani <yash@simplogics.com>
2024-12-08 21:28:43 +0530

    removed console messages

--- server/db/connectMongoDB.js
+++ server/db/connectMongoDB.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = async () =>{
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${connect.connection.host}`);
-    } catch(error){
-        console.log(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1);
-    }
-}
+const connectMongoDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    process.exit(1);
+  }
+};
 
 export default connectMongoDB;
